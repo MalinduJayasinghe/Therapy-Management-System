09d0eb53394302b74571b27fb1704ef906299b33
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-05-13 14:50:53 +0530

    added BOFactory code

--- src/main/java/lk/ijse/therapy_management_system/bo/BOFactory.java
+++ src/main/java/lk/ijse/therapy_management_system/bo/BOFactory.java
@@ -1,4 +1,27 @@
 package lk.ijse.therapy_management_system.bo;
 
+import lk.ijse.therapy_management_system.bo.custom.impl.*;
+
 public class BOFactory {
+
+    private static BOFactory boFactory;
+
+    private BOFactory() {
+    }
+
+    public static BOFactory getInstance() {
+        return boFactory == null ? (boFactory = new BOFactory()) : boFactory;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends SuperBO> T getBO(BOTypes boType) {
+        return switch (boType) {
+            case USER -> (T) new UserBOImpl();
+            case PATIENT -> (T) new PatientBOImpl();
+            case THERAPIST -> (T) new TherapistBOImpl();
+            case THERAPY_PROGRAM -> (T) new TherapyProgramBOImpl();
+            case THERAPY_SESSION -> (T) new TherapySessionBOImpl();
+            case PAYMENT -> (T) new PaymentBOImpl();
+        };
+    }
 }
