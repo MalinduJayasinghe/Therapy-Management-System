885d53faf34d9c487dbde1d895723c6eb92d4e94
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-05-13 14:42:48 +0530

    added FactoryConfiguration

--- src/main/java/lk/ijse/therapy_management_system/config/FactoryConfiguration.java
+++ src/main/java/lk/ijse/therapy_management_system/config/FactoryConfiguration.java
@@ -1,4 +1,44 @@
 package lk.ijse.therapy_management_system.config;
 
+import lk.ijse.therapy_management_system.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class FactoryConfiguration {
+
+    private static FactoryConfiguration factoryConfiguration;
+    private static SessionFactory sessionFactory;
+
+    private FactoryConfiguration() {
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+
+        sessionFactory = configuration
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Patient.class)
+                .addAnnotatedClass(Therapist.class)
+                .addAnnotatedClass(TherapyProgram.class)
+                .addAnnotatedClass(TherapySession.class)
+                .addAnnotatedClass(Payment.class)
+                .buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance() {
+        return factoryConfiguration == null ? (factoryConfiguration = new FactoryConfiguration()) : factoryConfiguration;
+    }
+
+    public Session openSession() {
+        return sessionFactory.openSession();
+    }
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public void shutdown() {
+        if (sessionFactory != null && !sessionFactory.isClosed()) {
+            sessionFactory.close();
+        }
+    }
 }
