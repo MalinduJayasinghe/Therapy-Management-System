c4db07f96c8c7ea09481901e49bfad298f27a6bb
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-05-17 15:32:07 +0530

    added DuplicateEntry Exception

---
+++ src/main/java/lk/ijse/therapy_management_system/exception/DuplicateEntryException.java
@@ -0,0 +1,5 @@
+package lk.ijse.therapy_management_system.exception;
+
+public class DuplicateEntryException extends SerenityException {
+    public DuplicateEntryException(String message) { super(message); }
+}
