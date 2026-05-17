ce6033a472f5c8ab25110ee5a2f460057febc422
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-05-17 14:54:11 +0530

    added UserDAO

--- src/main/java/lk/ijse/therapy_management_system/dao/custom/UserDAO.java
+++ src/main/java/lk/ijse/therapy_management_system/dao/custom/UserDAO.java
@@ -1,4 +1,12 @@
 package lk.ijse.therapy_management_system.dao.custom;
 
-public interface UserDAO {
+import lk.ijse.therapy_management_system.dao.CrudDAO;
+import lk.ijse.therapy_management_system.entity.User;
+
+import java.util.Optional;
+
+public interface UserDAO extends CrudDAO<User, String> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean updateCredentials(String userId, String newUsername, String newHashedPassword);
 }
