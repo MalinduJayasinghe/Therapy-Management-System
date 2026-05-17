9c50bad8b83233acc212b4bf3ee333b837890ae6
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-05-17 15:44:29 +0530

    added NotFoundException

---
+++ src/main/java/lk/ijse/therapy_management_system/exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package lk.ijse.therapy_management_system.exception;
+
+public class NotFoundException extends SerenityException {
+    public NotFoundException(String message) { super(message); }
+}
