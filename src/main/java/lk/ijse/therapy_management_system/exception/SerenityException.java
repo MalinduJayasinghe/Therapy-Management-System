5ef54b393606c691e68c612dd4bf00319a162846
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-05-17 15:34:27 +0530

    added Serenity Exception

---
+++ src/main/java/lk/ijse/therapy_management_system/exception/SerenityException.java
@@ -0,0 +1,10 @@
+package lk.ijse.therapy_management_system.exception;
+
+public class SerenityException extends RuntimeException {
+    public SerenityException(String message) {
+        super(message);
+    }
+    public SerenityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
