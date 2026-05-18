5e5624a62a184a57ce15bf9b6f34a54695a39b42
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-05-18 18:11:41 +0530

    added PatientTM

--- src/main/java/lk/ijse/therapy_management_system/dto/tm/PatientTM.java
+++ src/main/java/lk/ijse/therapy_management_system/dto/tm/PatientTM.java
@@ -1,4 +1,20 @@
 package lk.ijse.therapy_management_system.dto.tm;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class PatientTM {
+    private String patientId;
+    private String name;
+    private String nic;
+    private String email;
+    private String phone;
+    private String gender;
+    private LocalDate registrationDate;
 }
